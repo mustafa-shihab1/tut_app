f46e9af8d230564d39af2de48f154a4a026d585b
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-24 23:10:45 +0300

    create myApp - single instance

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/resources/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+
+  MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal();
+  factory MyApp() => _instance;
+
+
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
