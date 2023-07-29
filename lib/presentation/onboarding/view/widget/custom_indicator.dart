07c243b8cb0f5efb19d3410ee0578a6c8bb7c0ed
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-29 23:09:57 +0300

    refactor widgets

---
+++ lib/presentation/onboarding/view/widget/custom_indicator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:tut_app/domain/model/onboarding_models.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/constants_manager.dart';
+
+class CustomIndicator extends StatelessWidget {
+   final PageController pageController;
+   final SliderViewObject sliderViewObject;
+  const CustomIndicator({
+    super.key,
+     required this.pageController,
+    required this.sliderViewObject,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return SmoothPageIndicator(
+      effect: ColorTransitionEffect(
+        activeDotColor: ColorManager.primary,
+        dotHeight: AppConstants.dotHeight,
+        dotWidth: AppConstants.dotWidth,
+        spacing: AppConstants.dotSpacing,
+        dotColor: ColorManager.lightGrey,
+      ),
+      controller: pageController,
+      count: sliderViewObject.numOfSlides,
+    );
+  }
+}
