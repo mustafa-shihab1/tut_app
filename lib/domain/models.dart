9988f3d6ae7da1db3c4fae696a305f2889f7ddd2
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-18 18:55:23 +0300

    create auth models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -16,3 +16,25 @@ class SliderViewObject {
 }
 
 /// ****************************** [Login Model] ****************************** ///
+class Customer{
+  int id;
+  String name;
+  String numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts{
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication{
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
