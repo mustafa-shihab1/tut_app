ddba1e80efc9b9eaab0da65b34df335f5afa4c55
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-18 22:15:54 +0300

    create auth mapper

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,32 @@
+import 'package:tut_app/data/response/responses.dart';
+import 'package:tut_app/app/extensions.dart';
+import 'package:tut_app/domain/models.dart';
+
+extension CustomerResponseMapper on CustomerResponse{
+  Customer toDomain() {
+    return Customer(
+      id: id.onNull(),
+      name: name.onNull(),
+      numOfNotifications: numOfNotifications.onNull(),
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse{
+  Contacts toDomain() {
+    return Contacts(
+      phone: phone.onNull(),
+      email: email.onNull(),
+      link: link.onNull(),
+    );
+  }
+}
+
+extension AuthResponseMapper on AuthResponse{
+  Authentication toDomain() {
+    return Authentication(
+      customer: customer?.toDomain(),
+      contacts: contacts?.toDomain(),
+    );
+  }
+}
