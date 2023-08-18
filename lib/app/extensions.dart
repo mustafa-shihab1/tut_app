442ef396be56aa9dc0384895bac04ec498eac8d1
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-18 22:16:19 +0300

    fix bugs

--- lib/app/extensions.dart
+++ lib/app/extensions.dart
@@ -1,18 +1,37 @@
-extension NonNullableString on String? {
-  String orEmpty() {
-    if(this == null) {
+extension NonNullString on String? {
+  String onNull() {
+    if (this == null) {
       return '';
-    }else{
+    } else {
       return this!;
     }
   }
 }
 
-
-extension NonNullableInteger on int? {
-  int orZero() {
+extension NonNullDouble on double? {
+  double onNull() {
     if (this == null) {
       return 0;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullInt on int? {
+  int onNull() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullBoolean on bool? {
+  bool onNull() {
+    if (this == null) {
+      return false;
     } else {
       return this!;
     }
