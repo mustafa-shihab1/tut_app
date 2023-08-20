c4ec2db88aa05891c9920661351a6c50d7440499
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-20 23:37:26 +0300

    create errors extension

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,25 +1,60 @@
+import 'package:tut_app/data/network/failure.dart';
+
 enum DataSource {
   SUCCESS,
-  CREATED,
-  ACCEPTED,
-  CANCEL,
   NO_CONTENT,
   BAD_REQUEST,
-  UNAUTHORIZED,
   FORBIDDEN,
-  SEND_TIMEOUT,
-  UNKNOWN,
+  UNAUTHORIZED,
   NOT_FOUND,
-  RECIEVE_TIMEOUT,
-  CONNECT_TIMEOUT,
-  METHOD_NOT_ALLOWED,
-  CONFLICT,
   INTERNAL_SERVER_ERROR,
-  BAD_GATEWAY,
+  CONNECT_TIMEOUT,
+  CANCEL,
+  RECIEVE_TIMEOUT,
+  SEND_TIMEOUT,
   CACHE_ERROR,
-  SERVICE_UNAVAILABLE,
-  GATEWAY_TIMEOUT,
   NO_INTERNET_CONNECTION,
+  UNKNOWN,
+}
+
+extension DataSourceExtension on DataSource{
+  Failure getFailer(){
+    switch(this){
+case DataSource.SUCCESS:
+        return Failure(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+      case DataSource.NO_CONTENT:
+        return Failure(ResponseCode.NO_CONTENT, ResponseMessage.NO_CONTENT);
+      case DataSource.BAD_REQUEST:
+        return Failure(ResponseCode.BAD_REQUEST, ResponseMessage.BAD_REQUEST);
+      case DataSource.FORBIDDEN:
+        return Failure(ResponseCode.FORBIDDEN, ResponseMessage.FORBIDDEN);
+      case DataSource.UNAUTHORIZED:
+        return Failure(ResponseCode.UNAUTHORIZED, ResponseMessage.UNAUTHORIZED);
+      case DataSource.NOT_FOUND:
+        return Failure(ResponseCode.BAD_REQUEST, ResponseMessage.BAD_REQUEST);
+        case DataSource.UNKNOWN:
+        return Failure(ResponseCode.UNKNOWN, ResponseMessage.UNKNOWN);
+      case DataSource.INTERNAL_SERVER_ERROR:
+        return Failure(ResponseCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+      case DataSource.CONNECT_TIMEOUT:
+        return Failure(ResponseCode.CONNECT_TIMEOUT, ResponseMessage.CONNECT_TIMEOUT);
+      case DataSource.CANCEL:
+        return Failure(ResponseCode.CANCEL, ResponseMessage.CANCEL);
+      case DataSource.RECIEVE_TIMEOUT:
+        return Failure(ResponseCode.RECIEVE_TIMEOUT, ResponseMessage.RECIEVE_TIMEOUT);
+      case DataSource.SEND_TIMEOUT:
+        return Failure(ResponseCode.SEND_TIMEOUT, ResponseMessage.SEND_TIMEOUT);
+      case DataSource.CACHE_ERROR:
+        return Failure(ResponseCode.CACHE_ERROR, ResponseMessage.CACHE_ERROR);
+      case DataSource.NO_INTERNET_CONNECTION:
+        return Failure(ResponseCode.NO_INTERNET_CONNECTION, ResponseMessage.NO_INTERNET_CONNECTION);
+      case DataSource.UNKNOWN:
+        return Failure(ResponseCode.UNKNOWN, ResponseMessage.UNKNOWN);
+      default:
+        return Failure(ResponseCode.UNKNOWN, ResponseMessage.UNKNOWN);
+
+    }
+  }
 }
 
 class ResponseCode{
@@ -28,6 +63,7 @@ class ResponseCode{
   static const int BAD_REQUEST = 400;
   static const int UNAUTHORIZED = 401;
   static const int FORBIDDEN = 403;
+  static const int NOT_FOUND = 404;
   static const int INTERNAL_SERVER_ERROR = 500;
 
   static const int CONNECT_TIMEOUT = -1;
@@ -45,6 +81,7 @@ class ResponseMessage{
   static const String BAD_REQUEST = "Bad Request";
   static const String UNAUTHORIZED = "User is Unauthorized";
   static const String FORBIDDEN = "Forbidden";
+  static const String NOT_FOUND = "Not Found";
   static const String INTERNAL_SERVER_ERROR = "Internal Server Error";
 
   static const String CONNECT_TIMEOUT = "Connection Timeout";
