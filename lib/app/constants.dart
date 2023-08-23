67d76d27595d36683a028304defb56b6235a28c2
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-23 21:45:36 +0300

    add constants

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,4 +1,6 @@
 class Constants{
   static const String baseUrl = 'https://rvj22.wiremockapi.cloud';
   static const String loginPath = '/customers/login';
+  static const String token = "SEND TOKEN HERE";
+  static const int apiTimeOut = 60000;
 }
