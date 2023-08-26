3d30a9fbf9c1c276157607b006bba18014915484
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-26 20:39:01 +0300

    move file

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/forgot_password/forgot_password_view.dart';
-import 'package:tut_app/presentation/login/login_view.dart';
+import 'package:tut_app/presentation/login/view/login_view.dart';
 import 'package:tut_app/presentation/main/main_view.dart';
 import 'package:tut_app/presentation/onboarding/view/onboarding_view.dart';
 import 'package:tut_app/presentation/register/register_view.dart';
@@ -18,9 +18,9 @@ class Routes {
   static const String storeDetailsRoute = "/storeDetails";
 }
 
-class RouteGenerator{
-  static Route<dynamic> getRoute(RouteSettings settings){
-    switch(settings.name){
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
       case Routes.splashRoute:
         return MaterialPageRoute(builder: (_) => const SplashView());
       case Routes.loginRoute:
@@ -43,10 +43,10 @@ class RouteGenerator{
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
         builder: (_) => Scaffold(
-          appBar: AppBar(
-            title: const Text(AppStrings.noRouteFound),
-          ),
-          body: const Center(child: Text(AppStrings.noRouteFound)),
-        ));
+              appBar: AppBar(
+                title: const Text(AppStrings.noRouteFound),
+              ),
+              body: const Center(child: Text(AppStrings.noRouteFound)),
+            ));
   }
 }
