050fe51f404438c1fc128bf4eff3aa7819e6d4e2
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-28 23:23:56 +0300

    continue login view

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/login/viewmodel/login_viewmodel.dart';
 import 'package:tut_app/presentation/resources/assets_manager.dart';
 import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
 import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class LoginView extends StatefulWidget {
@@ -66,8 +67,36 @@ class _LoginViewState extends State<LoginView> {
                       stream: _viewModel.isOutputUserNameValid,
                       builder: (context, snapshot) {
                         return TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            controller: _userNameController);
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _userNameController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.username,
+                              labelText: AppStrings.username,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.usernameError),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.isOutputPasswordValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.password,
+                              labelText: AppStrings.password,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.passwordError),
+                        );
                       }),
                 )
               ],
