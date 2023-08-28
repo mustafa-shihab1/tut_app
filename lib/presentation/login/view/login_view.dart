bec8eaaab32671968f4c0de9cb155f32d58fccf9
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-08-28 23:21:28 +0300

    build login view

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/login/viewmodel/login_viewmodel.dart';
+import 'package:tut_app/presentation/resources/assets_manager.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class _LoginViewState extends State<LoginView> {
   LoginViewModel _viewModel = LoginViewModel();
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind(){
     _viewModel.start();
@@ -39,4 +43,38 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
   }
 
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                    child: Image(image: AssetImage(ImageAssets.splashLogo))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.isOutputUserNameValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _userNameController);
+                      }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
